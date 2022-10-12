c029a28f00bbb818406b0c4d8b0bae628ab33802
mx_in <mouxin1993@gmail.com>
2022-10-12 19:14:41 +0800

    feat: eslint

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Drum from './drum';
-import Root from './routers/root';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
-  Route,
-} from "react-router-dom";
-import ErrorPage from './error-page';
-import MarkdownPreview from './markdown-preview';
-import AppWrapper from './quote';
+  createBrowserRouter,
+} from 'react-router-dom'
+import Drum from './drum'
+import Root from './routers/root'
+
+import ErrorPage from './error-page'
+import MarkdownPreview from './markdown-preview'
+import AppWrapper from './quote'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
-    path: "drum",
-    element: <Drum />
+    path: 'drum',
+    element: <Drum />,
   }, {
-    path: "markdown",
-    element: <MarkdownPreview />
+    path: 'markdown',
+    element: <MarkdownPreview />,
   }, {
-    path: "quote",
-    element: <AppWrapper />
-  }
-]);
+    path: 'quote',
+    element: <AppWrapper />,
+  },
+])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
